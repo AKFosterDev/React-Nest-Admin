c67460addff77cbf9abb3f4ec4ec6dab7578e354
AKFosterDev <akfoster2015@gmail.com>
2021-02-06 06:09:30 -0800

    add foreign key to users for role_id

--- src/user/models/user.entity.ts
+++ src/user/models/user.entity.ts
@@ -1,6 +1,13 @@
 import { Options } from '@nestjs/common';
 import { Exclude } from 'class-transformer';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Role } from '../../role/role.entity';
 
 @Entity('users')
 export class User {
@@ -19,4 +26,8 @@ export class User {
   @Column()
   @Exclude()
   password: string;
+
+  @ManyToOne(() => Role)
+  @JoinColumn({ name: 'role_id' })
+  role: Role;
 }
