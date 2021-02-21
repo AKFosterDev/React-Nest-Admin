6aba193011f0e52fb0b749caab90c752572a094e
AKFosterDev <akfoster2015@gmail.com>
2021-02-21 11:34:15 -0800

    remove unneeded imports

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   app.setGlobalPrefix('api');
